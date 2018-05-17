a6c7c7fba7a4146786157f614adbeadfc2f2078e
Jason Dinsmore <jason@dinjas.com>
2018-05-17 16:52:18 -0700

    Add spec for `ApplicationHelper#app_name`

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+
+  describe '#app_name' do
+    it 'works' do
+      result = helper.app_name
+
+      expect(result).to eql('Dewts')
+    end
+  end
 end
